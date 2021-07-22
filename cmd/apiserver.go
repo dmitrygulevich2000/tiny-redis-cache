0c2cb835d5827c941b463424919220a04a3b28ad
Dmitry Gulevich <gulewi4@mail.ru>
2021-07-22 20:21:42 +0300

    port specification using command line argument

--- cmd/apiserver.go
+++ cmd/apiserver.go
@@ -3,14 +3,24 @@ package main
 import (
 	"github.com/dmitrygulevich2000/tiny-redis-cache/api/server"
 
+	"log"
 	"net/http"
+	"os"
+	"strconv"
 )
 
 func main() {
+	if len(os.Args) < 2 {
+		log.Fatalln("Expected server port in first arg")
+	}
+	port, err := strconv.Atoi(os.Args[1]) 
+	if err != nil {
+		log.Fatalln("Expected server port in first arg")
+	}
+	
 	srv := server.New()
-
 	server := http.Server {
-		Addr: ":9000",
+		Addr: ":" + strconv.Itoa(port),
 		Handler: srv,
 	}
 
